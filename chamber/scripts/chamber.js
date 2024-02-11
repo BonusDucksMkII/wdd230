a3dc7101ab6a33704f07fd777eec03d708da6da0
BonusDucksMkII <indifferential.equations@gmail.com>
2024-02-10 23:41:32 -0700

    test

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,10 @@
+const darkButton = document.querySelector("#dark-mode");
+darkButton.addEventListener("click", () => {
+    const main = document.querySelector("main");
+    main.classList.toggle('dark-mode');
+})
+
+// Date modified
 const dateModified = document.querySelector("#date");
 dateModified.innerHTML = `Last modified: ${document.lastModified}`;
 
